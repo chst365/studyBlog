68cc6751907eb1535b76b8f7adaeedc7f2930a42
chstz365 <chstz_365@163.com>
2021-07-08 00:25:53 +0800

    添加git使用

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,6 +2,13 @@
 module.exports = [
     { text: '首页', link: '/' },
     {
+        text: '开发必备',
+        link: '/basic/',
+        items: [
+            { text: 'Git', link: '/pages/7a6999/' },
+        ]
+    },
+    {
         text: '前端',
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
