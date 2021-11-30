7683dd7cc94f6dc732103f9e2ca8dec50a3534f5
chstz365 <chstz_365@163.com>
2021-11-30 23:43:15 +0800

    update matomo.js

--- docs/.vuepress/public/js/matomo.js
+++ docs/.vuepress/public/js/matomo.js
@@ -5,7 +5,7 @@ _paq.push(['enableLinkTracking']);
 (function () {
   var u = "//matomo.smart-lzgz.cn:443/";
   _paq.push(['setTrackerUrl', u + 'matomo.php']);
-  _paq.push(['setSiteId', '5']);
+  _paq.push(['setSiteId', '3']);
   var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
   g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
 })();
