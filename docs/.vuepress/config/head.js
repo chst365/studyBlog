beaf03397d747292ebc81ce50890b067ada73e51
张壮 <zhuangzhang@luoxin.cn>
2022-09-29 11:09:43 +0800

    updaet

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -12,9 +12,25 @@ module.exports = [
     ],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', size: '152x152', href: '/img/apple-touch-icon.png' }],
-    ['link', { rel: 'mask-icon', href: '/img/76x76.png', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/img/144x144.png' }],
+    [
+        "link",
+        { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
+    ],
+    [
+        "link",
+        {
+            rel: "mask-icon",
+            href: "/icons/safari-pinned-tab.svg",
+            color: "#3eaf7c",
+        },
+    ],
+    [
+        "meta",
+        {
+            name: "msapplication-TileImage",
+            content: "/icons/msapplication-icon-144x144.png",
+        },
+    ],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
     ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证
     ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
